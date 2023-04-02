7d93eb888fb62f6fe290fbeb364601ffc495c8c5
Şafak Can Baş <can.bas.cs@gmail.com>
2023-04-02 18:55:58 +0300

    Initial Commit

---
+++ MovieList/MovieListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieListApp.swift
+//  MovieList
+//
+//  Created by Can Bas on 2.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
